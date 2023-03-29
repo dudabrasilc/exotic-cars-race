9f6127faefc97ed1b3d5790a82fd5dbda8fb8e5a
Maria Coitinho <brasildu@gmail.com>
2023-03-29 02:30:14 -0500

    Create section tag for a car page and include link component that redirects the user to the car details

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -3,6 +3,12 @@ import { Link } from "react-router-dom";
 const CarCard = ({ car }) => {
   const { id, make, model, year, engine, power, image, torque, zero_to_sixty_mph, top_speed } = car
 
+return (
+  <section className="car-page">
+  <Link to={`/${id}/details`}> <img className="car-image" src={image} alt="car-image" /></Link>
+  <br></br>
+  <div className="bg-box">
+)
 
 
 }
