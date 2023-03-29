d1cb82dae3e3a72593412d08a8de8e38df12a2ce
Maria Coitinho <brasildu@gmail.com>
2023-03-29 02:40:03 -0500

    Import React built in tools and context from carsData. Pass down props to the CarsPage component

---
+++ src/components/CarsPage.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { CarsDataContext } from "../App";
+
+const CarsPage = ({ user, races, setRaces, addCarToRace }) => {
+
+
+};
+
+export default CarsPage;
