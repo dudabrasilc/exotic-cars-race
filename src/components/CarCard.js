0b9ea76be9bbc2024678b6ae11097d724a02072d
Maria Coitinho <brasildu@gmail.com>
2023-03-29 02:31:41 -0500

    Create car card tags displaying the destructured attributes from a car

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -8,6 +8,24 @@ return (
   <Link to={`/${id}/details`}> <img className="car-image" src={image} alt="car-image" /></Link>
   <br></br>
   <div className="bg-box">
+  <h1 className="car-title">{make} {model}</h1>
+                {/* <h3 className="car-subtitle">{model}</h3> */}
+                <div className="typebox">
+                    <small className="specs"><b>Year:</b> {year}</small>
+                    <br></br>
+                    <small className="specs"><b>Engine:</b> {engine}</small>
+                    <br></br>
+                    <small className="specs"><b>Power:</b> {power}</small>
+                    <br></br>
+                    <small className="specs"><b>Torque:</b> {torque}</small>
+                    <br></br>
+                    <small className="specs"><b>0-60mph:</b> {zero_to_sixty_mph}s</small>
+                    <br></br>
+                    <small className="specs"><b>Top speed:</b> {top_speed}mph</small>
+                </div>
+                <br></br>
+            </div>
+        </section>
 )
 
 
