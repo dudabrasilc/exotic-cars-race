51d4b91245f151f4392dcc0eb4b792647629e142
Maria Coitinho <brasildu@gmail.com>
2023-03-29 02:35:18 -0500

    Use setRace props to change state of a particular race once the component mounts. Create state variable of racesCars.

--- src/components/CarRaceImage.js
+++ src/components/CarRaceImage.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function CarRaceImage({ race, setRace, handleDeleteCar, show }) {
+  const [racesCars, setRacesCars] = useState([]);
+
+  useEffect(() => {
+    fetch(`/race/${race.id}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setRace(data);
+      });
+  }, []);
 
 }
 
