61b2860d11294dcd51df51d9158cf9468c6566bd
Maria Coitinho <brasildu@gmail.com>
2023-03-29 02:28:56 -0500

    Object destructurig of car for accessing ccar attributes

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 
 const CarCard = ({ car }) => {
+  const { id, make, model, year, engine, power, image, torque, zero_to_sixty_mph, top_speed } = car
+
+
 
 }
 
