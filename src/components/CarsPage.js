a9db840697c83ce66802a33ccbffa3a836465093
Maria Coitinho <brasildu@gmail.com>
2023-03-31 01:43:50 -0500

    Pass down props and create states for new data, fetch specific car with use effect, add conditional for if undefined data of a car

--- src/components/CarsPage.js
+++ src/components/CarsPage.js
@@ -3,7 +3,23 @@ import { useParams, useNavigate } from "react-router-dom";
 import { CarsDataContext } from "../App";
 
 const CarsPage = ({ user, races, setRaces, addCarToRace }) => {
+  const [selectedRace, setSelectedRace] = useState(user?.races[0]?.id);
+  let navigate = useNavigate();
+  const { id } = useParams();
+  const [carData, setCarData] = useState(null);
+  const { carsData, setCarsData } = useContext(CarsDataContext);
 
+  useEffect(() => {
+    fetch(`/cars/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCarData(data);
+      });
+  }, [id]);
+
+  if (!carData) {
+    return <div>Loading...</div>;
+  }
 
 };
 
