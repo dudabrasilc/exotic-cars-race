adc16cfbb18dbbe6763c7bedc10a8bc3eb95824f
Maria Coitinho <brasildu@gmail.com>
2023-03-31 01:49:37 -0500

    Create Collection component to represent collection of cars. Pass down props to this component and then export it

---
+++ src/components/Collection.js
@@ -0,0 +1,9 @@
+import CarCard from "./CarCard"
+import SearchBar from "./SearchBar"
+
+const Collection = ({ carsData, user, handleSearchAttributeChange, handleSearchTermChange, searchAttribute, searchTerm }) => {
+
+
+}
+
+export default Collection;
